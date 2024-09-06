ad223f5b4daa852d23da91ece435aa76151e9def
Renan Ameko <130808290+devRenanAmeko@users.noreply.github.com>
2024-09-06 12:54:20 -0300

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php
+defined('CONTROL') or die('acesso invalido');
+?>
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col text-center">
+            <h3 class="text-danger">Erro 404<br>Recurso não encontrado</h3>
+        </div>
+    </div>
+</div>
